73b01600ec914434b8c54eb768e7a6eee0b9a1af
ADMIN <221322@astanait.edu.kz>
2023-05-31 16:03:07 +0600

    created Vertex class and added fields

---
+++ src/Vertex.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    private double distance;
+
+
+
+}
