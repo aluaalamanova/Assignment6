45d3bb726aeeec01d39d99aa1774ba2941a108bb
ADMIN <221322@astanait.edu.kz>
2023-05-31 18:20:43 +0600

    bfs() search method

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -8,5 +8,27 @@ public class BreadthFirstSearch<V> extends Search<V> {
     public BreadthFirstSearch(WeightedGraph<V> graph) {
         super(graph);
     }
+    public void bfs(Vertex<V> startVertex) {
+        Set<Vertex<V>> visited = new HashSet<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
 
+        startVertex.setDistance(0.0);
+
+        visited.add(startVertex);
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            System.out.println("Visited: " + currentVertex.getData() + ", Distance: " + currentVertex.getDistance());
+
+            List<Vertex<V>> neighbors = graph.getNeighbors(currentVertex);
+            for (Vertex<V> neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    neighbor.setDistance(currentVertex.getDistance() + 1);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 }
