7f9a70e922f72f96219fef4b644f7e34d6bcea91
ADMIN <221322@astanait.edu.kz>
2023-05-31 18:16:04 +0600

    BreadthFirstSearch class and constructor

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,2 +1,12 @@
-public class BreadthFirstSearch {
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class BreadthFirstSearch<V> extends Search<V> {
+    public BreadthFirstSearch(WeightedGraph<V> graph) {
+        super(graph);
+    }
+
 }
