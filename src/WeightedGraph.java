d219cef5992e5e19b73ef13c2ab793cc303f5f99
ADMIN <221322@astanait.edu.kz>
2023-05-31 16:59:29 +0600

    WeightedGraph class with fields and constructor

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,3 +1,14 @@
-public class WeightedGraph<Vertex> {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Vertex<V>>> adjacencyMap;
+
+    public WeightedGraph() {
+        this.adjacencyMap = new HashMap<>();
+    }
+
 
 }
